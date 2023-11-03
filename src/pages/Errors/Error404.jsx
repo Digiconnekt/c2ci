947eb5665f637b4b16e8d3abd9ca378802beaf60
Rohan Daddekar <rohandaddekar9@gmail.com>
2023-11-03 13:26:56 +0530

    error page created

--- src/pages/Errors/Error404.jsx
+++ src/pages/Errors/Error404.jsx
@@ -5,10 +5,12 @@ import { NavLink } from "react-router-dom";
 const Error404 = () => {
   return (
     <>
-      <div className="bg-gradient-to-br from-gray-600 via-gray-800 to-gray-950 h-screen flex flex-col items-center justify-center">
+      <div className="bg-gradient-to-br from-gray-600 via-gray-800 to-gray-950 h-screen flex flex-col items-center justify-center px-4">
         <h1 className="text-9xl font-bold text-white">404</h1>
-        <p className="text-3xl text-white mt-4">We couldn't find that page</p>
-        <p className="text-md text-gray-300 mt-2">
+        <p className="text-3xl text-white mt-4 text-center">
+          We couldn't find that page
+        </p>
+        <p className="text-md text-gray-300 mt-2 text-center">
           Sorry mate, the page you're looking for doesn't exists.
         </p>
         <NavLink
