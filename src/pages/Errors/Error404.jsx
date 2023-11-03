7edbf617c28756503bb35886710fd661b4ea3c5b
Rohan Daddekar <rohandaddekar9@gmail.com>
2023-11-03 13:24:12 +0530

    error page created

--- src/pages/Errors/Error404.jsx
+++ src/pages/Errors/Error404.jsx
@@ -1,5 +1,25 @@
+/* eslint-disable react/no-unescaped-entities */
+
+import { NavLink } from "react-router-dom";
+
 const Error404 = () => {
-  return <>Error404</>;
+  return (
+    <>
+      <div className="bg-gradient-to-br from-gray-600 via-gray-800 to-gray-950 h-screen flex flex-col items-center justify-center">
+        <h1 className="text-9xl font-bold text-white">404</h1>
+        <p className="text-3xl text-white mt-4">We couldn't find that page</p>
+        <p className="text-md text-gray-300 mt-2">
+          Sorry mate, the page you're looking for doesn't exists.
+        </p>
+        <NavLink
+          to="/"
+          className="mt-6 py-4 px-6 bg-c-black hover:bg-c-blue-dark text-white rounded-lg hover:underline border border-c-blue-dark transition"
+        >
+          Back to Home
+        </NavLink>
+      </div>
+    </>
+  );
 };
 
 export default Error404;
