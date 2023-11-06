17d23a5564db4c5f619c727caf56fa0f79172943
Rohan Daddekar <rohandaddekar9@gmail.com>
2023-11-06 17:12:29 +0530

    added typing effect on hero section

--- src/pages/Home/partials/HeroSection.jsx
+++ src/pages/Home/partials/HeroSection.jsx
@@ -1,24 +1,30 @@
+import { TypeAnimation } from "react-type-animation";
+
 const HeroSection = () => {
   return (
     <>
       <section
         className="hero"
         style={{
-          backgroundImage:
-            "url(https://img.freepik.com/free-photo/futuristic-smart-city-with-5g-global-network-technology_53876-98438.jpg?w=1380&t=st=1699010416~exp=1699011016~hmac=a41e0bee17001e1f05add0c6655b731b5aad475661e4da4a4e6deda5fa274395)",
+          backgroundImage: "url(../../../images/home/hero.jpg)",
           height: "calc(100vh - 88px)",
         }}
       >
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-white">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
+          <div className="max-w-5xl">
+            <TypeAnimation
+              sequence={[
+                "Your Partner for Seamless IT Solutions",
+                1000,
+                "Your Partner for Seamless IT Services",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+              className="text-5xl lg:text-7xl md:text-6xl font-bold leading-snug"
+            />
           </div>
         </div>
       </section>
