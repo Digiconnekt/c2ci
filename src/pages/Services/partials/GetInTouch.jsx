e4d5f3730c11a1472ae5dbfa7a11ea0af8c13ef0
Rohan Daddekar <rohandaddekar9@gmail.com>
2023-11-07 18:31:26 +0530

    update service page with animation

--- src/pages/Services/partials/GetInTouch.jsx
+++ src/pages/Services/partials/GetInTouch.jsx
@@ -1,4 +1,6 @@
 import { NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
+import { fadeInLeft, fadeInRight } from "../../../helpers/framerMotion";
 
 const GetInTouch = () => {
   return (
@@ -7,16 +9,26 @@ const GetInTouch = () => {
         style={{
           backgroundImage: "url(../../../images/services/services-banner.png)",
         }}
-        className="py-16 bg-c-blue-dark bg-no-repeat bg-cover"
+        className="py-16 bg-c-blue-dark bg-no-repeat bg-cover overflow-hidden"
       >
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-12 items-center md:gap-10 gap-y-10">
-            <div className="col-span-12 lg:col-span-6">
+            <motion.div
+              variants={fadeInLeft}
+              initial="hidden"
+              whileInView="visible"
+              className="col-span-12 lg:col-span-6"
+            >
               <h2 className="text-4xl md:text-5xl text-white font-semibold md:leading-snug">
                 Grow your business on your own terms using C2Ci
               </h2>
-            </div>
-            <div className="col-span-12 lg:col-span-6">
+            </motion.div>
+            <motion.div
+              variants={fadeInRight}
+              initial="hidden"
+              whileInView="visible"
+              className="col-span-12 lg:col-span-6"
+            >
               <p className="text-white">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem
                 laudantium tempore omnis minima nulla, corporis est dicta porro
@@ -28,7 +40,7 @@ const GetInTouch = () => {
               >
                 Get In Touch
               </NavLink>
-            </div>
+            </motion.div>
           </div>
         </div>
       </section>
